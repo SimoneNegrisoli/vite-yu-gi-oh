b532f4d4448b310c331acefc8f2413e2a2bf7684
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-11-13 16:43:16 +0100

    store cards

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 import { store } from './data/store.js'
 import HeaderComp from './components/Header/HeaderComp.vue';
 import MainComp from './components/Main/MainComp.vue';
+import axios from 'axios';
 export default {
   name: 'App',
   components: {
@@ -17,6 +18,18 @@ export default {
     return {
       store
     }
+  },
+  methods: {
+    getCards() {
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then((response) => {
+        store.cards = response.data
+      })
+      console.log(store.cards)
+    }
+
+  },
+  created() {
+    this.getCards()
   }
 }
 </script>
