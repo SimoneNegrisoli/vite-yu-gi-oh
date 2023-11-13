1fb8c73fb536de2e306417865c6e7c66cf48a234
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-11-13 18:52:44 +0100

    card layout

--- src/components/Main/CardComp.vue
+++ src/components/Main/CardComp.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card" style="width: 18rem;">
+    <div class="card">
         <img :src="image" class="card-img-top" :alt="title">
         <div class="card-body">
             <h5 class="card-title">{{ title }}</h5>
@@ -11,14 +11,31 @@
 </template>
 
 <script>
+
 export default {
     name: 'CardComp',
-    props: [
-        image = String,
-        title = String,
-        text = String
-    ]
+    props: {
+        image: 'String',
+        title: 'String',
+        text: 'String'
+    }
+
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../../assets/styles/partials/variables.scss' as *;
+
+div {
+    .card-body {
+        height: 120px;
+        background-color: $colororange ;
+    }
+
+    h5 {
+        font-size: 1rem;
+        font-weight: bolder;
+        color: white;
+    }
+}
+</style>
--- src/components/Main/MainComp.vue
+++ src/components/Main/MainComp.vue
@@ -1,8 +1,13 @@
 <template>
     <main>
-        <div v-for="(card, index) in store.cards">
-            <CardComp :image="card.card_images[0].image_url" />
+        <div class="container">
+            <div class="row">
+                <div v-for="(card, index) in store.cards" class=" col-12 col-md-2 text-center my-3 mx-1 ">
+                    <CardComp :image="card.card_images[0].image_url" :title="card.name" :text="card.archetype" />
+                </div>
+            </div>
         </div>
+
     </main>
 </template>
 
@@ -22,4 +27,10 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.row {
+    background-color: white;
+    padding: 50px;
+    justify-content: center;
+}
+</style>
